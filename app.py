d7d2aae8800b955205ed018520061e0a5d4a78a8
Abraham0034 <aethra002@gmail.com>
2026-08-14 23:54:28 -0400

    corregir importacion de app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 from flask import Flask, render_template, request, send_file, jsonify
 import os
 from PIL import Image
